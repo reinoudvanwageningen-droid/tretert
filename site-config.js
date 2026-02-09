f236122eeaf5a4de924ef2e4290c6fa1745c468f
reinoudvanwageningen-droid <reinoudvanwageningen@gmail.com>
2026-02-09 20:14:02 +0100

    Redesign CAPS Consult landing pages

---
+++ site-config.js
@@ -0,0 +1,25 @@
+window.CAPS_CONFIG = {
+  company: {
+    name: "[Bedrijfsnaam]",
+    tagline: "[Korte tagline / positionering]",
+    legalName: "[Statutaire bedrijfsnaam]",
+    address: "[Straat + huisnummer]",
+    postalCode: "[Postcode]",
+    city: "[Plaats]",
+    country: "[Land]",
+    kvk: "[KvK-nummer]",
+    vat: "[BTW-nummer]",
+    phone: "[Telefoonnummer]",
+    email: "[E-mailadres]",
+    linkedin: "[LinkedIn URL]",
+    website: "[Website URL]"
+  },
+  contact: {
+    primaryCta: "Plan kennismaking",
+    secondaryCta: "Bekijk projecten",
+    ctaLink: "contact.html#formulier"
+  },
+  social: {
+    ogImage: "[URL naar Open Graph afbeelding]"
+  }
+};
